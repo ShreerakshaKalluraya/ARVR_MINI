67cc341ba03a7caa11b14c5a6856d53d9cc0f9e5
ShreerakshaKalluraya <shreerakshabaddadka@gmail.com>
2024-11-02 12:32:52 +0530

    speed_and_position_changes

--- src/Scene2.js
+++ src/Scene2.js
@@ -26,8 +26,8 @@ const TreadmillModel = ({ positionZ }) => {
 
     useFrame(() => {
         if (ref.current) {
-            ref.current.position.z += 0.01; // Move the treadmill forward
-            if (ref.current.position.z > 20) { // Reset position when out of view
+            ref.current.position.z += 0.005; // Move the treadmill forward
+            if (ref.current.position.z >25) { // Reset position when out of view
                 ref.current.position.z = positionZ; // Reset to initial position
             }
         }
@@ -38,7 +38,7 @@ const TreadmillModel = ({ positionZ }) => {
             ref={ref}
             object={treadmill.scene}
             scale={[0.2, 0.2, 0.2]}
-            position={[5, -1, positionZ]}
+            position={[5.5, -0.6, positionZ]}
         />
     );
 };
@@ -58,12 +58,12 @@ const Lights = () => {
 const Scene = () => {
     return (
         <Canvas style={{ height: '100vh', backgroundColor: '#f0f0f0' }} shadows>
-            <PerspectiveCamera makeDefault position={[0, 2, 5]} fov={75} near={0.1} far={1000} />
+            <PerspectiveCamera makeDefault position={[0, 0, 0.1]} fov={75} near={0.1} far={1000} />
             <Lights />
             {/* Render multiple TreadmillModel instances with different initial z positions */}
+            <TreadmillModel positionZ={-9} />
             <TreadmillModel positionZ={-10} />
-            <TreadmillModel positionZ={-10} />
-            <TreadmillModel positionZ={-10} />
+            <TreadmillModel positionZ={-9} />
             <OrbitControls maxPolarAngle={Math.PI / 2} enableDamping={true} dampingFactor={0.25} />
         </Canvas>
     );
